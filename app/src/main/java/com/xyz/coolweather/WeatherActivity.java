c23f0b9248d892031a6816653f905eb70a24a2b7
misterzxy <yuanzx163@163.com>
2018-10-26 17:36:50 +0800

    Update Project 1026-2

--- app/src/main/java/com/xyz/coolweather/WeatherActivity.java
+++ app/src/main/java/com/xyz/coolweather/WeatherActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
